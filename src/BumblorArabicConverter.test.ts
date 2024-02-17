860cb6c9017fb80bd2cabf69ed69a51a35382a06
ashussein <ahussein@wpi.edu>
2024-02-17 18:08:29 -0500

    updates

--- src/BumblorArabicConverter.test.ts
+++ src/BumblorArabicConverter.test.ts
@@ -1,81 +1,90 @@
 import {expect, test} from "vitest";
+import {BumblorArabicConverter} from "./BumblorArabicConverter.ts";
+
 
 test('Correct Input Upper case',() => {
    const input = 'MMMMDCCCCXXXXIIII';
-   const BumblorArabicConverter  = new BumblorArabicConverter();
-   expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(4944);
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    // console.log(converter.outOfOrder(input.toLowerCase()));
+   expect(converter.bumblor2arabic(input)).toBe(4944);
 });
 
 test('Correct Input Lower Case',() => {
     const input = 'MMMMDCCCCXXXXIIII'.toLowerCase();
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(4944);
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(converter.bumblor2arabic(input)).toBe(4944);
 });
 
+// test('D L V Appear more than once', () => {
+//     const input = 'DDX';
+//     const converter: BumblorArabicConverter = new BumblorArabicConverter();
+//     expect(converter.bumblor2arabic(input)).toThrowError('Malformed Number');
+// })
 test('D L V Appear more than once', () => {
     const input = 'DDX';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
-})
+    const converter = new BumblorArabicConverter();
+    expect(() => converter.bumblor2arabic(input)).toThrowError('Malformed Number');
+});
+
 
 test('M C X I Appear more than 4 times', () => {
     const input = 'MMDDCCCCC';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(() => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Out of Order 1', () => {
     const input = 'MMMCM';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect( () => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Out of Order 2', () => {
     const input = 'IVXCDM';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect( () => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Out of Order 3', () => {
     const input = 'MMMMDCCIX';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect( () => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Not Included Syntax Upper', () => {
     const input = 'MMMMDCCK';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(() => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Not Included Syntax Lower', () => {
     const input = 'MMMMDCCk';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(() => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Out of Bounds Smaller Than 1 (empty string)', () => {
     const input = '';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(() => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Leading Space', () => {
     const input = ' MMDDCCCCC';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter  = new BumblorArabicConverter();
+    expect(() => converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Middle Space', () => {
     const input = 'MMDDC CCCC';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(() =>  converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('Traling Space', () => {
     const input = 'MMDDCCCCC ';
-    const BumblorArabicConverter  = new BumblorArabicConverter();
-    expect(BumblorArabicConverter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
+    const converter: BumblorArabicConverter = new BumblorArabicConverter();
+    expect(converter.bumblor2arabic(input)).toBe(Error('Malformed Number'));
 })
 
 test('FLoating point', () => {
---
+++ src/BumblorArabicConverter.ts
@@ -0,0 +1,82 @@
+
+export class BumblorArabicConverter{
+    map: Map<string, number>
+
+    constructor() {
+        this.map = new Map<string, number>();
+        this.map.set('m',1000);
+        this.map.set('d',500);
+        this.map.set('c',100);
+        this.map.set('l',50);
+        this.map.set('x',10);
+        this.map.set('v',5);
+        this.map.set('i',1);
+
+    }
+    dlv(bumblor: string){
+        const isD = bumblor.split('d').length-1;
+        const isL = bumblor.split('l').length-1;
+        const isV = bumblor.split('v').length-1;
+        return (isD<=1 && isV<=1 && isL<=1);
+
+    }
+    mcxi(bumblor: string){
+        const isM = bumblor.split('m').length-1;
+        const isC = bumblor.split('c').length-1;
+        const isX = bumblor.split('x').length-1;
+        const isI = bumblor.split('i').length-1;
+        return (isM<=4 && isC<=4 && isX<=4 && isI<=4);
+    }
+
+    outOfOrder(bumblor: string){
+        for(let i=0; i<(bumblor.length-1); i++){
+            if(this.map.get(bumblor[i])! < this.map.get(bumblor[i+1])!){
+                console.log(this.map.get(bumblor[i]), this.map.get(bumblor[i+1]));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    notIncluded(bumblor: string){
+        for(let i=0; i<(bumblor.length); i++){
+            if(!Array.from(this.map.keys()).includes(bumblor[i])){
+                return false;
+            }
+    }
+        return true;
+    }
+
+
+        spaces(bumblor: string){
+        return(!bumblor.includes(" "));
+    }
+
+
+    bumblor2arabic(bumblor: string): number {
+        bumblor = bumblor.toLowerCase();
+        if(this.dlv(bumblor) && this.mcxi(bumblor) && this.notIncluded(bumblor) && this.spaces(bumblor) && this.outOfOrder(bumblor)){
+            let total = 0;
+            for(let i=0; i<bumblor.length; i++){
+                total += this.map.get(bumblor[i])!;
+            }
+            return total;
+        }
+        else{
+            throw Error("Malformed Number")
+        }
+    }
+
+    floatPoint(arabic: number){
+        return(Number.isInteger(arabic));
+    }
+
+    outOfBounds(arabic: number){
+
+        return(1>=arabic && arabic<=4999);
+    }
+    // arabic2bumblor(arabic: number): string {
+    //
+    // }
+    
+}
